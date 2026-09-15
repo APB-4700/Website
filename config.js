e7f94ae5648bbbaeee45bb9d802cdb19632fcffb
APB-4700 <madshund970@gmail.com>
2026-09-15 10:31:30 +0200

    config.js

--- config.js
+++ config.js
@@ -4,21 +4,7 @@ const siteConfig = {
 
     om: "Dette er vores private side til os og vores venner.",
 
-    info: [
-        {
-            titel: "Server",
-            tekst: "Serveren kører 24/7."
-        },
-        {
-            titel: "Version",
-            tekst: "Version 1.0"
-        },
-        {
-            titel: "Lokation",
-            tekst: "Danmark 🇩🇰"
-        }
-    ],
-
+    
     links: [
         {
             navn: "Discord",
