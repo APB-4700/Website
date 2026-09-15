1d9ab691acb418b473f9e5b8ff2c6f4338162e50
APB-4700 <madshund970@gmail.com>
2026-09-15 10:25:32 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,32 @@
+const siteConfig = {
+    navn: "APB",
+    status: "Online",
+
+    om: "Dette er vores private side til os og vores venner.",
+
+    info: [
+        {
+            titel: "Server",
+            tekst: "Serveren kører 24/7."
+        },
+        {
+            titel: "Version",
+            tekst: "Version 1.0"
+        },
+        {
+            titel: "Lokation",
+            tekst: "Danmark 🇩🇰"
+        }
+    ],
+
+    links: [
+        {
+            navn: "Discord",
+            url: "https://discord.com/"
+        },
+        {
+            navn: "GitHub",
+            url: "https://github.com/APB-4700/Website"
+        }
+    ]
+};
